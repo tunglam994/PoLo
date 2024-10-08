97599721f6784fb39336c888c9fd37380134a363
onlyzabao <ntgbao.235@gmail.com>
2024-10-08 22:16:30 +0700

    script: test run script

--- run.sh
+++ run.sh
@@ -9,9 +9,10 @@ while IFS= read -r line || [ -n "$line" ]; do
   params="$params $name $val"
 done <"$file_path"
 export PYTHONPATH="."
-gpu_id=1
+# gpu_id=1
 cmd="python mycode/model/trainer.py $params"
 
 echo "Executing $cmd"
 
-CUDA_VISIBLE_DEVICES=$gpu_id $cmd
+# CUDA_VISIBLE_DEVICES=$gpu_id $cmd
+$cmd
