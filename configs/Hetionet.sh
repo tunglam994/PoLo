cc67933086d87a86417845ee0c69ca91dd60b52a
Hoang Hung Manh <100746478+hungmanh21@users.noreply.github.com>
2024-10-22 06:45:54 +0700

    Update Hetionet.sh

--- configs/Hetionet.sh
+++ configs/Hetionet.sh
@@ -1,5 +1,5 @@
-input_dir="/kaggle/input/polo-datasets/DRKG_DB_20_10/"
-base_output_dir="/kaggle/output/DRKG_20_10/"
+input_dir="/kaggle/input/polo-datasets/hetionet_21_10/"
+base_output_dir="/kaggle/output/hetionet_21_10/"
 total_iterations=1000
 eval_every=10
 patience=2
