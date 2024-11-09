4f4d924290385ff822fabb4c438f9590a9667edb
tunglam994 <125526310+tunglam994@users.noreply.github.com>
2024-11-09 12:29:13 +0700

    Update Hetionet.sh

--- configs/Hetionet.sh
+++ configs/Hetionet.sh
@@ -1,5 +1,5 @@
-input_dir="/kaggle/input/polo-new-data/DDI_Polo_new/option1/"
-base_output_dir="/kaggle/output/option1/"
+input_dir="/kaggle/input/polo-new-data/DDI_Polo_new/option2/"
+base_output_dir="/kaggle/output/option2/"
 total_iterations=1000
 eval_every=10
 patience=2
